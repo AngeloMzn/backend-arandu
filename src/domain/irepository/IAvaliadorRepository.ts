051f38ef8f7d5b63999efba3bacd3dbb860def9a
GustaEncinas <dev.gustavo.encinas@gmail.com>
2025-06-09 00:24:02 -0400

    [FEAT][BACKEND-12]: adicionando findById na interface do avaliador

--- src/domain/irepository/IAvaliadorRepository.ts
+++ src/domain/irepository/IAvaliadorRepository.ts
@@ -3,4 +3,5 @@ import { Avaliador } from "../entities/Avaliador";
 export interface IAvaliadorRepository{
     findByCPF(CPF: string): Promise<Avaliador | null>;
     save(avaliador: Avaliador): Promise<Avaliador>;
+    findById(id: number): Promise<Avaliador | null>;
 }
